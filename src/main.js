8294b63ff3ebf8c16968938dd672d72c10bcc32f
Cody Stanish <112568028+StanishCode@users.noreply.github.com>
2024-12-25 01:09:18 -0500

    create events section logic, call to action section, footer, and image shadows

--- src/main.js
+++ src/main.js
@@ -5,6 +5,36 @@ import dineLogo from "/dine.svg";
 // import { setupCounter } from "./counter.js";
 
 document.querySelector("#dine-logo").innerHTML = `<img src="${dineLogo}" />`;
+document.querySelector(".footer-logo").innerHTML = `<img src="${dineLogo}" />`;
+
+const eventSelections = document.querySelectorAll(".events-selection");
+const eventImages = document.querySelectorAll(".events-image");
+const eventDescriptions = document.querySelectorAll(".event-description");
+
+const activateSelection = (selected) => {
+  for (let image of eventImages) {
+    image.classList.remove("events-image--active");
+  }
+  for (let description of eventDescriptions) {
+    description.classList.remove("event-description--active");
+  }
+  for (let selection of eventSelections) {
+    selection.classList.remove("events-selection--active");
+  }
+
+  const current = selected.closest("div");
+  eventImages[+current.dataset.id].classList.add("events-image--active");
+  eventDescriptions[+current.dataset.id].classList.add(
+    "event-description--active"
+  );
+  current.classList.add("events-selection--active");
+};
+
+eventSelections.forEach((selection) =>
+  selection.addEventListener("click", () => {
+    activateSelection(event.target);
+  })
+);
 
 // document.querySelector("#app").innerHTML = `
 //   <div>
